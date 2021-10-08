1f90f42a6dec6261d715c6b9aace3a77b1ec8e73
Sebastian Rosende <peperosende1515@gmail.com>
2021-10-08 20:11:29 -0300

    Actualización db

--- todolist_/database/db_tasks.sql
+++ todolist_/database/db_tasks.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-09-2021 a las 01:01:11
--- Versión del servidor: 10.1.40-MariaDB
--- Versión de PHP: 7.3.5
+-- Tiempo de generación: 09-10-2021 a las 01:08:52
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `db_tasks`
 --
-CREATE DATABASE IF NOT EXISTS `db_tasks` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `db_tasks`;
 
 -- --------------------------------------------------------
 
@@ -33,9 +30,9 @@ USE `db_tasks`;
 CREATE TABLE `task` (
   `id` int(11) NOT NULL,
   `titulo` varchar(50) NOT NULL,
-  `descripcion` text,
-  `prioridad` int(11) NOT NULL DEFAULT '3',
-  `finalizada` tinyint(1) NOT NULL DEFAULT '0'
+  `descripcion` text DEFAULT NULL,
+  `prioridad` int(11) NOT NULL DEFAULT 3,
+  `finalizada` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -43,12 +40,29 @@ CREATE TABLE `task` (
 --
 
 INSERT INTO `task` (`id`, `titulo`, `descripcion`, `prioridad`, `finalizada`) VALUES
-(1, 'Estudiar Web 2', 'Bla bla bla', 5, 0),
 (2, 'Estudiar TMC', 'bla bla', 2, 0),
 (3, 'test 3', 'test 3 ', 1, 0),
 (4, 'hola', 'decrip', 5, 0),
-(5, 'tarea desde el form', 'es', 4, 0),
-(6, 'sdfssdf', 'sdfsd', 1, 0);
+(5, 'tarea desde el form', 'es', 4, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`) VALUES
+(1, 'admin@tudai', '$2y$10$UILAmrV/sO3ah4PxeIDKTOPewYzWY/2aF.9rR02E8E8KmvYDIoEuW');
 
 --
 -- Índices para tablas volcadas
@@ -61,6 +75,12 @@ ALTER TABLE `task`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -69,6 +89,12 @@ ALTER TABLE `task`
 --
 ALTER TABLE `task`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
